f3154c0dcc7b431371f83745a5e6a03591ed578a
Tech-A43 <tech-a43@Tech-A43s-MacBook-Air.local>
2017-12-18 14:17:01 +0000

    put code into functions

--- js/app.js
+++ js/app.js
@@ -1,20 +1,27 @@
 
 $(document).ready(function() {
 
-	var safePath = [];
+	
 
-	for (var i = 0; i < 36; i++) {
-		var div = $('<div class="box"></div>');
-		$("#game-container").append(div);
+	function makeGrid() {
+		for (var i = 0; i < 36; i++) {
+			var div = $('<div class="box"></div>');
+			$("#game-container").append(div);
+		}	
 	}
 
-	$(".box").one("click", function() {
-		console.log("Click!");
-		reveal(this);	
-	});
+	function addListeners() {
+		$(".box").one("click", function() {
+			console.log("Click!");
+			reveal(this);	
+		});
+	}
 
 	function reveal(box) {
 		box.innerHTML = "hello";
 	}
+
+	makeGrid();
+	addListeners();
 })
 	
