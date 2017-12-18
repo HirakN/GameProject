e72deb2f20a6393c8b757738dde2b23687340c31
Tech-A43 <tech-a43@Tech-A43s-MacBook-Air.local>
2017-12-18 13:43:36 +0000

    added reveal function call to click listeners

--- js/app.js
+++ js/app.js
@@ -1,17 +1,20 @@
 
 $(document).ready(function() {
 
+	var safePath = [];
+
 	for (var i = 0; i < 36; i++) {
 		var div = $('<div class="box"></div>');
 		$("#game-container").append(div);
 	}
 
 	$(".box").one("click", function() {
-		console.log("Click!")	
+		console.log("Click!");
+		reveal(this);	
 	});
 
-	function reveal() {
-		
+	function reveal(box) {
+		box.innerHTML = "hello";
 	}
 })
 	
