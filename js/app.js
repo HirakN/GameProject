1d916feca0d7e09a974329c45603220b343c71c3
Tech-A43 <tech-a43@Tech-A43s-MacBook-Air.local>
2017-12-18 15:26:44 +0000

    removed 2 boxes from grid and addes them sepearetly again as start box and finish box

--- js/app.js
+++ js/app.js
@@ -4,12 +4,14 @@ $(document).ready(function() {
 	var clearChance = 0.6;
 
 	function makeGrid() {
+		// Starting point
 		var div = $('<div id="start-box"></div>');
 		$("#game-container").append(div);
 		for (var i = 0; i < 34; i++) {
 			var div = $('<div class="box"></div>');
 			$("#game-container").append(div);
 		}
+		// Finish line
 		var div = $('<div id="end-box"></div>');
 		$("#game-container").append(div);
 	}
